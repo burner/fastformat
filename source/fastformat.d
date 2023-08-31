dbfd7223efebd424c1cd0a5ed909c40cb064e07e
Robert Schadek <rschadek@symmetryinvestments.com>
2023-08-31 16:18:50 +0100

    f*** you int propagation

--- source/fastformat.d
+++ source/fastformat.d
@@ -30,16 +30,58 @@ class FFormatException : Exception {
 
 private:
 
-void fformattedWriteImpl(ref Array array, FFormatSpec spec, ulong value) {
-	static const char[71] cs = 
-		"zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz";
+static const char[71] numChars = 
+	"zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz";
 
+void fformattedWriteImpl(ref Array array, FFormatSpec spec, long value) {
 	int base = 10;
+	long tmp_value;
 
     do {
-        ulong tmp_value = value;
+        tmp_value = value;
         value /= base;
-		array.put(cs[35 + (tmp_value - value * base)]);
+		array.put(numChars[35 + (tmp_value - value * base)]);
+    } while(value);
+
+	if(tmp_value < 0) {
+		array.put('-');
+	}
+
+	foreach(idx; 0 .. array.pos / 2) {
+		char tmp = array.buf[array.pos - idx - 1];
+		array.buf[array.pos - idx - 1] = array.buf[idx];
+		array.buf[idx] = tmp;
+	}
+}
+
+unittest {
+	Array arr;
+
+	fformattedWriteImpl(arr, FFormatSpec.init, -1337L);
+	assert(arr.pos == 5);
+	assert(arr.buf[0 .. 5] == "-1337");
+
+	arr.reset();
+
+	fformattedWriteImpl(arr, FFormatSpec.init, -13370L);
+	assert(arr.pos == 6);
+	assert(arr.buf[0 .. 6] == "-13370");
+
+	arr.reset();
+
+	fformattedWriteImpl(arr, FFormatSpec.init, 13370L);
+	assert(arr.pos == 5);
+	assert(arr.buf[0 .. 5] == "13370");
+}
+
+void fformattedWriteImpl(ref Array array, FFormatSpec spec, ulong value) {
+	int base = 10;
+	ulong tmp_value;
+
+    do {
+        tmp_value = value;
+        value /= base;
+		array.put(numChars[35 + (tmp_value - value * base)]);
     } while(value);
 
 	foreach(idx; 0 .. array.pos / 2) {
@@ -52,13 +94,13 @@ void fformattedWriteImpl(ref Array array, FFormatSpec spec, ulong value) {
 unittest {
 	Array arr;
 
-	fformattedWriteImpl(arr, FFormatSpec.init, 1337);
+	fformattedWriteImpl(arr, FFormatSpec.init, 1337UL);
 	assert(arr.pos == 4);
 	assert(arr.buf[0 .. 4] == "1337");
 
 	arr.reset();
 
-	fformattedWriteImpl(arr, FFormatSpec.init, 13370);
+	fformattedWriteImpl(arr, FFormatSpec.init, 13370UL);
 	assert(arr.pos == 5);
 	assert(arr.buf[0 .. 5] == "13370");
 }
